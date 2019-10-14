309aa27c164fccb5dc10e7c04e3f0fd292331714
Piggey <49120531+Piggey@users.noreply.github.com>
2019-10-14 12:26:46 +0200

    ver1.1
    made it to usable state I think

--- tracker.py
+++ tracker.py
@@ -2,14 +2,14 @@ from time import sleep
 from requests import get
 from bs4 import BeautifulSoup
 
-match = KDA = ''
+KDA = ''
 test_url = 'https://euw.op.gg/summoner/userName=GIGAKLOCJUNGLER'
 url = input('op.gg link: ')
 if(url == 'test'):
     url = test_url
 
 Won = input("Matches already won (type 'l' to load previous Win/Loss stats from stats.txt file): ")
-if(Won == 'l'):
+if(Won == 'l' or Won == "L"):
     stat = open("stats.txt").read().split("/")
     Won = int(stat[0])
     Lost = int(stat[1])
@@ -22,16 +22,17 @@ Interval = int(input("Minutes of interval inbetween checking: "))
 
 def CheckMatches():
     #### Disa powinno sie jebac ####
-    global match
     global KDA
-    prev_match = match
     prev_KDA = KDA
     page = get(url)
     sup = BeautifulSoup(page.content, 'html.parser')
-    match = sup.find('div', {'class': 'GameResult'}).get_text().strip('\n																				 ')
-    KDA = sup.find('span', {'class': 'KDARatio'}).get_text()
+    try:
+        match = sup.find('div', {'class': 'GameResult'}).get_text().strip('\n																				 ')
+        KDA = sup.find('span', {'class': 'KDARatio'}).get_text()
+    except:
+        print("No new matches found!")
 
-    if(prev_KDA != KDA and prev_match != match):
+    if(prev_KDA != KDA):
         print("New activity has been tracked.")
         if(match == "Victory"):
             return (1,0)
@@ -40,13 +41,14 @@ def CheckMatches():
     else:
         return (0,0)
 
-
-
 def CheckLeague():
     page = get(url)
     soup = BeautifulSoup(page.content, 'html.parser')
-    tier = soup.find('div', {'class': 'TierRank'}).get_text()
-    LP = soup.find('span', {'class': 'LeaguePoints'}).get_text().strip('\n				 LP\n			')
+    try:
+        tier = soup.find('div', {'class': 'TierRank'}).get_text()
+        LP = soup.find('span', {'class': 'LeaguePoints'}).get_text().strip('\n				 LP\n			')
+    except:
+        return 'UNR'
 
     return f'{tier[0]} {LP}LP'
 
